6a779b567a743cb30648433cf897e6024c65378f
Corso330Z <joao.corso@estudante.ifro.edu.br>
2025-02-18 23:44:43 -0300

    save

--- rate/rate/src/app/paginas/AdcAtor/page.jsx
+++ rate/rate/src/app/paginas/AdcAtor/page.jsx
@@ -1,30 +1,27 @@
 import React from 'react';
-import styles from './adcAtor.module.css'
+import styles from './adcAtor.module.css';
 import FormMenor from '../../components/FormMenor';
 
 const AdcAtor = () => {
-
-//   const goBack = () => {
-//     window.history.back();
-//   };
-
+  //   const goBack = () => {
+  //     window.history.back();
+  //   };
   return (
-    <div className={styles.cards}>
-      <div className={styles.seta_voltar}>
-        <button className={styles.back_button} >&#8592;</button>
+    <div className={styles.cartoes}>
+      <div className={styles.setaVoltar}>
+        <button className={styles.botaoVoltar}>&#8592;</button>
       </div>
-      <div className={styles.container}>
-        <h2 className={styles.h2}>ADICIONAR ATOR</h2>
-        <div className={styles.form_container}>
-          <img src="img/ator.webp" alt="Ator" className={styles.ator_image} />
-          <FormMenor/>
-          <form className={styles.atorForm}>
-            
-            <div className={styles.button_group}>
+      <div className={styles.conteiner}>
+        <h2 className={styles.titulo}>ADICIONAR ATOR</h2>
+        <div className={styles.formConteiner}>
+          <img src="img/ator.webp" alt="Ator" className={styles.imagemAtor} />
+          <FormMenor />
+          <form className={styles.formAtor}>
+            <div className={styles.grupoBotoes}>
               <a href="vizu_mi_sugs.html">
-                <button type="button" className={styles.cance}>Cancelar</button>
+                <button type="button" className={styles.botaoCancelar}>Cancelar</button>
               </a>
-              <button type="submit" className={styles.add}>Adicionar</button>
+              <button type="submit" className={styles.botaoAdicionar}>Adicionar</button>
             </div>
           </form>
         </div>
